197bb062f7f1a4bc8ed3cdfe123f5683d44ff64a
siakan1107 <121685349+siakan1107@users.noreply.github.com>
2023-04-23 22:06:46 +0900

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -18,9 +18,19 @@ resource "aws_iam_user_policy" "user_policy" {
 
 resource "aws_iam_user_policy" "user_policy_2" {
   user   = aws_iam_user.user.name
-  policy_arn = data.aws_iam_policy.user_policy_b.arn
+  
+    policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "*"
+            ],
+            "Resource": [
+                "*"
+            ]
+        }
+    ]
 }
-
-data "aws_iam_policy" "user_policy_b" {
-  arn = "arn:aws:iam::aws:policy/AdministratorAccess"  
-}  
